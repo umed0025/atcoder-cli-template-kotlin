c8ca3b607e5640381ed29c1193aeef86167b6b24
umed0025 <umed0025@gmail.com>
2022-07-16 21:44:49 +0900

    modify main function.

--- kotlin/src/main/kotlin/Main.kt
+++ kotlin/src/main/kotlin/Main.kt
@@ -1,7 +1,12 @@
-fun main() {
-    val a = readLine()!!.toInt()
-    val (b, c) = readLine()!!.split(" ").map { it.toInt() }
-    val s = readLine()!!
+fun main(args: Array<String>) {
+    var lines = mutableListOf<String>()
+    var line: String?
+    while (readLine().also { line = it } != null) {
+        lines.add(line!!)
+    }
+    val a = lines[0]!!.toInt()
+    val (b, c) = lines[1]!!.split(" ").map { it.toInt() }
+    val s = lines[2]!!
     print((a + b + c).toString() + " " + s)
     println()
 }
